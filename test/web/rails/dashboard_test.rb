a7a345891ce808fdf3b9b4cbbeffd014ba3feab9
Phill Baker <phillbaker@retrodict.com>
2018-08-04 11:35:28 -0400

    Update travis for rails 51,52.

--- test/web/rails/dashboard_test.rb
+++ test/web/rails/dashboard_test.rb
@@ -60,14 +60,14 @@ class RailsDashboardTest < ActionController::TestCase
   # --  Actions used in non-admin actions, e.g. in JS --
 
   def test_add_participant
-    xhr :post, :add_participant, :v => 'food=0'
+    post :add_participant, :params => { :v => 'food=0' }, :xhr => true
     assert_response :success
     assert @response.body.blank?
     assert_equal 1, experiment(:food).alternatives.map(&:participants).sum
   end
 
   def test_add_participant_multiple_experiments
-    xhr :post, :add_participant, :v => 'food=0,drink=1'
+    post :add_participant, :params => { :v => 'food=0,drink=1' }, :xhr => true
     assert_response :success
     assert @response.body.blank?
     assert_equal 1, experiment(:food).alternatives.map(&:participants).sum
@@ -76,18 +76,18 @@ class RailsDashboardTest < ActionController::TestCase
 
   def test_add_participant_with_invalid_request
     @request.user_agent = 'Googlebot/2.1 ( http://www.google.com/bot.html)'
-    xhr :post, :add_participant, :v => 'food=0'
+    post :add_participant, :params => { :v => 'food=0' }, :xhr => true
     assert_equal 0, experiment(:food).alternatives.map(&:participants).sum
   end
 
   def test_add_participant_no_params
-    xhr :post, :add_participant
+    post :add_participant, :xhr => true
     assert_response :not_found
     assert @response.body.blank?
   end
 
   def test_add_participant_not_fail_for_unknown_experiment
-    xhr :post, :add_participant, :e => 'unknown=0'
+    post :add_participant, :params => { :e => 'unknown=0' }, :xhr => true
     assert_response :not_found
     assert @response.body.blank?
   end
@@ -114,19 +114,19 @@ class RailsDashboardTest < ActionController::TestCase
   end
 
   def test_complete_forces_confirmation
-    xhr :post, :complete, :e => "food", :a => 0
+    post :complete, :params => { :e => "food", :a => 0 }, :xhr => true
     assert_response :success
     assert @response.body =~ /#{ CGI.unescape({ :confirmed => 0 }.to_query) }/
   end
 
   def test_complete_with_confirmation_completes
-    xhr :post, :complete, :e => "food", :a => 0, :confirmed => 'true'
+    post :complete, :params => { :e => "food", :a => 0, :confirmed => 'true' }, :xhr => true
     assert_response :success
     assert !Vanity.playground.experiment(:food).active?
   end
 
   def test_chooses
-    xhr :post, :chooses, :e => "food", :a => 0
+    post :chooses, :params => { :e => "food", :a => 0 }, :xhr => true
     assert_response :success
   end
 end
